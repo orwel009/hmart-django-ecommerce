25f63af686b3c09be6ba0841f13233f86a37048e
orwel009 <orwelvincent1@gmail.com>
2022-07-26 09:15:50 +0530

    first commit

--- Emart/settings.py
+++ Emart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e)eoc)gz03ldo#i!s7ud3o-ci2tat+jius4m3e+erw&^obz!pi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
